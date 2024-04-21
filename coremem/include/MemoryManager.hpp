753cc5c80f65b739344ec5e290c7b537e9dd0af6
Nikolai Z <thesiriuss@gmail.com>
2024-04-21 10:23:08 +0200

    Added:stacked memory manager to track the order of allocations and provide meory leak control tool.

---
+++ coremem/include/MemoryManager.hpp
@@ -0,0 +1,93 @@
+#pragma once
+#include <StackAllocator.hpp>
+
+#include <cstdio>
+#include <cassert>
+#include <vector>
+#include <list>
+
+namespace coremem
+{
+/*
+mechanism to track stack memory allocations and deallocate them in the correct
+order - stack allocator based. 
+*/
+class MemoryManager
+{
+public:
+  static constexpr size_t MEMORY_CAPACITY = 134217728; // 128 MB;
+
+  MemoryManager();
+  ~MemoryManager();
+
+  MemoryManager(const MemoryManager&) = delete;
+  MemoryManager& operator=(MemoryManager&) = delete;
+
+  inline void* Allocate(size_t memSize, const char* user = nullptr)
+  {
+    printf(
+        "%s allocated %d bytes of global memory.",
+        user != nullptr ? user : "Unknown", memSize);
+    void* pMemory = m_MemoryAllocator->allocate(memSize, alignof(uint8_t));
+
+    this->m_PendingMemory.push_back(
+        std::pair<const char*, void*>(user, pMemory));
+
+    return pMemory;
+  }
+
+  inline void Free(void* pMem)
+  {
+    if (pMem == this->m_PendingMemory.back().second)
+    {
+      printf(
+          "%s freed global memory.", m_PendingMemory.back().first != nullptr
+                                         ? m_PendingMemory.back().first
+                                         : "Unknown");
+
+      this->m_MemoryAllocator->free(pMem);
+      this->m_PendingMemory.pop_back();
+
+      bool bCheck = true;
+      while (bCheck == true)
+      {
+        bCheck = false;
+
+        // do not report already freed memory blocks.
+        for (auto it : this->m_FreedMemory)
+        {
+          if (it == this->m_PendingMemory.back().second)
+          {
+            printf(
+                "%s freed global memory.",
+                m_PendingMemory.back().first != nullptr
+                    ? m_PendingMemory.back().first
+                    : "Unknown");
+
+            this->m_MemoryAllocator->free(it);
+            this->m_PendingMemory.pop_back();
+            this->m_FreedMemory.remove(it);
+
+            bCheck = true;
+            break;
+          }
+        }
+      };
+    }
+    else { this->m_FreedMemory.push_back(pMem); }
+  }
+
+  void CheckMemoryLeaks();
+
+private:
+  // Pointer to global allocated memory
+  void* m_GlobalMemory;
+
+  // Allocator used to manager memory allocation from global memory
+  StackAllocator* m_MemoryAllocator;
+
+  std::vector<std::pair<const char*, void*>> m_PendingMemory;
+
+  std::list<void*> m_FreedMemory;
+};
+} // namespace coremem
--- corevu_test/mem_sys_test.hpp
+++ corevu_test/mem_sys_test.hpp
@@ -1,29 +1,51 @@
 #pragma once
-#include <iostream>
+
+#include <coremem/include/MemoryManager.hpp>
+
 #include <coremem/include/LinearAllocator.hpp>
 #include <coremem/include/StackAllocator.hpp>
 #include <coremem/include/PoolAllocator.hpp>
 
+#include <iostream>
+#include <chrono>
+
+#include <vector>
+#include <array>
+
 namespace corevutest
 {
-// virtual page size?
+// virtual page size? Pages are typically 512 to 8192 bytes, with 4096 being a
+// typical value.
+
+struct Mfoo
+{
+  uint8_t first;
+  // std::vector<uint8_t> second;
+  std::array<uint16_t, 4> third;
+};
 
 class MemSysTest
 {
 public:
   void run()
   {
-    /*use stack allocator for systems, linear allocator for temp objects - clear before frame end, pool allocator for objects(entities,components)*/
+    /* have one on-stack memory api class which has global allocation in
+     * constructor and gives access to linear and stack allocations. And tracks
+     memory leaks. Have a
+     * factory which provides chunc allocators, based on pool allocators.
+     Provide a proxy allocator to track and debug memory.*/
 
+    /*use stack allocator for systems, linear allocator for temp objects - clear
+     * before frame end, pool allocator for objects(entities,components)*/
 
     int init_stack;
     init_stack = 4256;
     int* init_heap = static_cast<int*>(malloc(sizeof(int)));
     *init_heap = 0x7FFFFFFF;
-    
+
     free(init_heap);
 
-    void* global_mem = malloc(16); // bytes
+    void* global_mem = malloc(18000); // bytes
     if (global_mem == nullptr) return;
 
     std::cout << "Stack(" << &init_stack << ") Heap(" << global_mem << ")"
@@ -59,33 +81,87 @@ public:
     }
 
     // pool test
-    if (true)
+    if (false)
     {
       class Obj
       {
       public:
-        Obj(uint32_t in):inside_d{in} {};
-      //private:
+        Obj(uint32_t in) : inside_d{in} {};
+        // private:
         uint32_t inside_d = 0;
         uint32_t inside_d2 = 0;
       };
 
       coremem::PoolAllocator pool_alloc(
-          16, global_mem, sizeof(Obj), alignof(Obj));
-      //auto sofob = sizeof(Obj);
-      //auto aofob = alignof(Obj);
-     // auto* mem = pool_alloc.allocate(sizeof(Obj), alignof(Obj));
+          18000, global_mem, sizeof(Obj), alignof(Obj));
+      auto sofob = sizeof(Obj);
+      auto aofob = alignof(Obj);
+      auto* mem = pool_alloc.allocate(sizeof(Obj), alignof(Obj));
 
-      //Obj* obj = new (mem) Obj(0xEFFFFFFF);
-      //obj->inside_d = 1;
-      //obj->~Obj();
+      Obj* obj = new (mem) Obj(0xEFFFFFFF);
+      obj->inside_d = 1;
+      obj->~Obj();
+      pool_alloc.free(mem);
 
+      using namespace std::chrono;
+      {
+        auto start = high_resolution_clock::now();
 
-     // pool_alloc.free(mem);
+        for (size_t i = 0; i < 100000; i++)
+        {
+          auto* ob = new Obj(12);
+          // delete ob;
+        }
+
+        std::cout << duration_cast<microseconds>(
+                         high_resolution_clock::now() - start)
+                         .count()
+                  << "microsec" << std::endl;
+      }
+
+      {
+        auto start = high_resolution_clock::now();
+
+        for (size_t i = 0; i < 100000; i++)
+        {
+          auto* mem_l = pool_alloc.allocate(sizeof(Obj), alignof(Obj));
+          auto* obj_l = new (mem_l) Obj(12);
+          // obj_l->~Obj();
+          // pool_alloc.free(mem_l);
+        }
+
+        std::cout << duration_cast<microseconds>(
+                         high_resolution_clock::now() - start)
+                         .count()
+                  << "microsec" << std::endl;
+      }
     }
 
     free(global_mem);
     global_mem = nullptr;
+
+    void* addit_mem = malloc(64);
+    auto addit_size = sizeof(Mfoo);
+    auto addit = new (addit_mem) Mfoo();
+
+    addit->first = 0xFFFF;
+
+    addit->third.at(0) = 0xFFFF;
+    addit->third.at(3) = 0xFFFF;
+
+    {
+      coremem::MemoryManager mm;
+      uint32_t* v1 = static_cast<uint32_t*>(mm.Allocate(8, "first"));
+      *v1 = 0xFFFFFFFF;
+      auto* v2 = mm.Allocate(8, "second");
+      auto* v3 = mm.Allocate(8, "third");
+      auto* v4 = mm.Allocate(8, "fourth");
+
+      mm.Free(v4);
+      mm.Free(v2);
+      mm.Free(v3);
+      mm.Free(v1);
+    }
 
     std::cout << "end" << std::endl;
   }
